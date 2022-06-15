282a34205a01c3703a5bec59450ab2fcf56d5e6d
bayufm <85687061+bayufm@users.noreply.github.com>
2022-06-15 12:50:00 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,7 +15,7 @@
   global.autoReadGc = true // Ubah Ke false Jika Chat Di Grup Tidak Ingin Di Baca Oleh Bot
   global.autoReadAll = false // Ubah Ke true Jika Ingin Seluruh Chat Di Baca Oleh Bot
   global.anticall = true // Ubah Ke false Jika Tidak Ingin Bot Blockir Yang Telepon
-  global.available = false // Ubah Ke false Jika Bot Mu Tidak Ingin Terlihat Online
+  global.available = true // Ubah Ke false Jika Bot Mu Tidak Ingin Terlihat Online
   global.autoTyping = false // Ubah Ke true Jika Ingin Saat Ada Yang Chat Bot Auto Mengetik
   global.autoRecord = true // Ubah Ke false Jika Tidak Ingin Saat Ada Yang Chat Bot Auto Merekam
   global.multiplier = 69 // Semakin Besar Angkanya Semakin Sulit Untuk Naik Level
@@ -23,18 +23,18 @@
   global.prefa = ['','.','/','#']
 
   // Other
-  global.ownername = 'Riy' // Ganti Jadi Namamu
-  global.owner = ['6281575886399','6281575886399','6285813334446'] // Kalo Mau Lebih Tambahin Aja
-  global.ownernomer = '6281575886399' // Owner Utama Istilahnya
-  global.botname = 'Hinata Hyuga' // Ganti Jadi Nama Botmu
-  global.packname = 'Hinata Hyuga' // Sticker Weem
-  global.author = 'Riy' // Sticker Weem
-  global.webme = 'https://api-riy.herokuapp.com' // Ganti Sama Web Mu
-  global.linkgroup = 'https://chat.whatsapp.com/HZhLynp6E9QKNrRFtUp26T' // Ganti Sama Link Gc Mu
+  global.ownername = 'Bayxz' // Ganti Jadi Namamu
+  global.owner = ['6289506312533'] // Kalo Mau Lebih Tambahin Aja
+  global.ownernomer = '6289506312533' // Owner Utama Istilahnya
+  global.botname = 'SyinXD' // Ganti Jadi Nama Botmu
+  global.packname = 'YahahXD' // Sticker Weem
+  global.author = 'XD' // Sticker Weem
+  global.webme = 'https://minecraftpocket-servers.com/server/118409/' // Ganti Sama Web Mu
+  global.linkgroup = 'https://chat.whatsapp.com/IrCtQD8TzwF9jjifYhQFmU' // Ganti Sama Link Gc Mu
   global.sessionName = 'session'
-  global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw' // Ganti Sama Link Yt Mu
-  global.github = 'https://github.com/riychdwayne' // Ganti Sama Link Github Mu
-  global.instagram = 'https://instagram.com/riychdwayne' // Ganti Sama Link Ig mu
+  global.youtube = 'https://youtube.com/channel/UCvR7ud3DmDhFfQtn15gr4nQ' // Ganti Sama Link Yt Mu
+  global.github = 'private yages' // Ganti Sama Link Github Mu
+  global.instagram = '-' // Ganti Sama Link Ig mu
 
   // Background Setiap Menu
   global.bg1 = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
@@ -57,13 +57,13 @@
   // Edit Sesuai Kemauanmu
   global.mess = {
   admin: '_Lu Admin? 🤨_',
-  botAdmin: '_Bot Bukan Admin 😔_',
+  botAdmin: '_Bot Belum dijadiin admin GBLK_',
   owner: '_Lu Bukan Owner Gw 😠_',
   group: '_Khusus Group 😏_',
-  private: '_Khusus Private Chat_',
+  private: '_Khusus Private Chat🗿_',
   bot: '_Khusus Pengguna Nomer Bot_',
   wait: '_Sabar Sedang Di Proses ..._',
-  done: '_Done Ngab 🤗_',
+  done: '_Done Ngab 🗿_',
   }
 
   // Kalo Mau Menyesuaikan Nama File Foto Nya
